a4093f9e76dbca835673b37341b8b2349cda1d73
yourgotocoder <yourgotocoder@gmail.com>
2023-04-03 10:20:39 +0530

    Putting each plugin into their respective file

---
+++ plugins/nvim_gomove.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "booperlv/nvim-gomove",
+    config = {
+      map_defaults = true,
+      reindent = true,
+      undojoin = true,
+      move_paste_end_col = false,
+    },
+  },
+}
