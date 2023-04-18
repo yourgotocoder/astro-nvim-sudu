f73147719a3357c0531ec2c79fadd82e7eab99d7
yourgotocoder <yourgotocoder@gmail.com>
2023-04-18 11:13:54 +0530

    Fixing codeium errors

--- plugins/codeium.lua
+++ plugins/codeium.lua
@@ -8,6 +8,6 @@ return {
       vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
       vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
     end,
-    event = "User AstroFile"
+    event = "User AstroFile",
   }
 }
